45eca8727f84e0b8e4a0fddf8539e0f5680269d0
christopherkumar <160341137+christopherkumar@users.noreply.github.com>
2024-02-18 03:41:00 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageDraw
 
 load_css()
 
-#<editor-fold desc="Display Picture">
 col1, col2, col3 = st.columns(3)
 # Load the image
 img = Image.open('dp.png').convert("RGBA")
@@ -28,8 +27,7 @@ result.paste(img, (0, 0), mask)
 
 # Display the circular image
 col2.image(result, use_column_width=True)
-#</editor-fold>
-#<editor-fold desc="Bio">
+
 st.header('Christopher Vishnu Kumar', anchor=None)
 
 # Using Markdown to center text within a rectangle with rounded edges
@@ -51,11 +49,9 @@ st.markdown("""
     University of the South Pacific - Foundation Science Programme
 </div>
 """, unsafe_allow_html=True)
-#</editor-fold>
 
 icon_size = 20
 
-#<editor-fold desc="Buttons">
 # Custom CSS to center the buttons and add spacing
 st.markdown("""
 <style>
@@ -97,7 +93,3 @@ st.markdown("""
     <a href="{4}" target="_blank"><img src="{5}" /></a>
 </div>
 """.format(links[0], img_urls[0], links[1], img_urls[1], links[2], img_urls[2], unsafe_allow_html=True)
-# st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-# st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-# st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
-#</editor-fold>
