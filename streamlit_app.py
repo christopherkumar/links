d338ad3ddb4310d9db26b301d3c7009e204d8a50
christopherkumar <160341137+christopherkumar@users.noreply.github.com>
2024-02-18 03:14:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageDraw
 
 load_css()
 
+#<editor-fold desc="Display Picture">
 col1, col2, col3 = st.columns(3)
 # Load the image
 img = Image.open('dp.png').convert("RGBA")
@@ -27,9 +28,9 @@ result.paste(img, (0, 0), mask)
 
 # Display the circular image
 col2.image(result, use_column_width=True)
-
+#</editor-fold>
 st.header('Christopher Vishnu Kumar')
-
+#<editor-fold desc="Bio">
 # Custom HTML and CSS to style the info box
 info_box_html = """
 <div style="
@@ -43,15 +44,18 @@ info_box_html = """
     Bachelor of Engineering (Honours) - Computer Systems Engineering
 </div>
 """
+#</editor-fold>
 
 st.markdown(info_box_html, unsafe_allow_html=True)
 
 icon_size = 20
 
-#st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-#st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-#st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-#st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-#st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-#st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-#st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+#<editor-fold desc="Buttons">
+# st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
+# st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
+# st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
+# st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
+# st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
+# st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
+# st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+#</editor-fold>
